0b802d037ff4a47679c424f8916592db206ccf12
bibhuti <bhusan.bibhutimohapatra@gmail.com>
2023-11-08 12:59:11 +0530

    First and last position

--- searching/generic_binary_search.py
+++ searching/generic_binary_search.py
@@ -31,13 +31,3 @@ def binary_search(input_list, target, get_item_position):
             start = mid+1
 
     return -1
-
-
-asc_num_list = [5, 6, 25, 38, 54, 88, 94]
-desc_num_list = [94, 88, 54, 38, 25, 6, 5]
-item = 5
-asc_item_result = binary_search(asc_num_list, item, get_item_position_in_ascending_list)
-desc_item_result = binary_search(desc_num_list, item, get_item_position_in_descending_list)
-
-print("Ascending list item position:", asc_item_result)
-print("Descending list item position:", desc_item_result)
--- searching/problems/start_n_end_position.py
+++ searching/problems/start_n_end_position.py
@@ -1,10 +1,35 @@
-from test_cases import search_test_cases
+from searching.generic_binary_search import binary_search
+
+
+def get_first_position(input_list, mid_pos, target):
+    if input_list[mid_pos] == target and mid_pos != 0 and input_list[mid_pos-1] == target:
+        return "left"
+    if input_list[mid_pos] == target:
+        return "found"
+    elif input_list[mid_pos] > target:
+        return "left"
+    else:
+        return "right"
+
+
+def get_last_position(input_list, mid_pos, target):
+    if input_list[mid_pos] == target and mid_pos != len(input_list) - 1 and input_list[mid_pos+1] == target:
+        return "right"
+    if input_list[mid_pos] == target:
+        return "found"
+    elif input_list[mid_pos] > target:
+        return "left"
+    else:
+        return "right"
 
 
 def find_start_end_position_of(a_number, in_sorted_array):
-    return a_number
+    first_pos = binary_search(in_sorted_array, a_number, get_first_position)
+    last_pos = binary_search(in_sorted_array, a_number, get_last_position)
+    return first_pos, last_pos
 
 
-sorted_list = [7, 7, 7, 8, 12]
-item_to_search = 7
+sorted_list = [7, 8, 9, 10, 10]
+item_to_search = 8
 result = find_start_end_position_of(item_to_search, sorted_list)
+print(result)
